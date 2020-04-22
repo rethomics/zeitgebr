0a16c5eb13e8fa546370b6178c64a4e1729a7e81
Quentin Geissmann <qgeissmann@gmail.com>
2020-04-22 15:00:36 -0700

    also update spectrogram code regarding #11

--- R/spectrogram.R
+++ R/spectrogram.R
@@ -44,29 +44,30 @@ spectrogram <- function(var,
                         FUN = cwt_spectrogram,
                         ...){
 
-  n_val = var__ = id = . = .N = t0 = .SD = NULL
+  n_val = var__ = key = . = .N = t0 = .SD = NULL
 
   var_of_interest <- deparse(substitute(var))
   regular_data <- resample(data, var_of_interest, resample_rate)
-
+  key <- data.table::key(regular_data)
   data.table::setnames(regular_data, var_of_interest, "var__")
 
   reg_data_nval <- regular_data[, .(n_val = length(unique(var__))),
-                                by = c(data.table::key(regular_data))]
+                                by = c(key)]
 
-  invalid <- reg_data_nval[n_val < 2, id]
+  invalid <- reg_data_nval[n_val < 2][[key]]
+
   if(length(invalid) > 0)
     warning(sprintf("Removing individuals that have only one unique value for `val`:\n%s",paste(invalid, sep="\n")))
 
-  regular_data <- regular_data[!(id %in% invalid)]
+  regular_data <- regular_data[!(key %in% invalid)]
   out <- regular_data[, FUN(var__,
                             period_range = period_range,
                             sampling_rate = resample_rate,
                             ...),
-                      by = c(data.table::key(data))]
+                      by = c(key)]
 
-  time_origin <- data[, .(t0 = .SD[1,t]),by=id]
-  out[, t:= out[time_origin, on="id"][,t +t0]]
+  time_origin <- data[, .(t0 = .SD[1,t]),by=c(key)]
+  out[, t:= out[time_origin, on=key][,t +t0]]
   out
 }
 
